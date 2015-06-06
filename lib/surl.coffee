7be8a24ea4871f03c6092d94bf1c014b75208e25
Lee <gould.lee@googlemail.com>
2015-06-07 00:35:19 +0100

    Added encode function

--- lib/surl.coffee
+++ lib/surl.coffee
@@ -11,4 +11,15 @@ class SUrl
     return darr.reduce (id, current) ->
                   (id * BASE) + ALPHABET.indexOf(current)
 
+  encode: (id) ->
+    console.log(id)
+    encoded = ''
+    while id > 0
+      console.log(id)
+      encoded += ALPHABET[id % BASE]
+      id = parseInt(id / BASE)
+
+    console.log('Return: ' + encoded)
+    return encoded
+
 module.exports = SUrl
--- tests/surl.test.js
+++ tests/surl.test.js
@@ -5,8 +5,10 @@ var assert = require("assert")
 var SUrl = require("../lib/surl");
 
 describe('SUrl', function(){
+    var surl = null
+
     describe('#decode()', function(){
-        var surl = null
+
         it('should return the decoded string.', function(){
             surl = new SUrl();
             assert.equal(surl.decode('b'), 1);
@@ -22,4 +24,20 @@ describe('SUrl', function(){
             assert.equal(surl.decode('bb'), 63);
         })
     })
+
+    describe('#encode()', function(){
+
+        it('should return the encoded id.', function(){
+            surl = new SUrl();
+            assert.equal(surl.encode('1'), 'b');
+        })
+
+        it('should return the encoded large id.', function(){
+            surl = new SUrl();
+            assert.equal(surl.encode('125'), 'bc');
+        })
+    })
 })
+
+
+
